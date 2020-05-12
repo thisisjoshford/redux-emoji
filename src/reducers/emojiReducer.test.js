4988a3911c8fec0e2a70cc1f7fe9db81356c4042
Josh Ford <thisisjoshford@gmail.com>
2020-05-12 16:17:51 -0700

    eats a snack test!

--- src/reducers/emojiReducer.test.js
+++ src/reducers/emojiReducer.test.js
@@ -1,15 +1,18 @@
 import emojiReducer from './emojiReducer';
-import { drinkCoffee } from '../actions/emojiActions';
+import { drinkCoffee, eatSnack } from '../actions/emojiActions';
 
 describe('emoji reducers', () => {
   it('handles drink coffee action', () => {
-    const state = {
-      coffee: 0
-    };
+    const state = { coffee: 0 };
     const drink1Coffee = drinkCoffee();
-
     const newState = emojiReducer(state, drink1Coffee);
-
     expect(newState).toEqual({ coffee: 1 });
+  });
+
+  it('handles eat a snack action', () => {
+    const state = { snacks: 0 };
+    const eatASnack = eatSnack();
+    const newState = emojiReducer(state, eatASnack);
+    expect(newState).toEqual({ snacks: 1 });
   });
 });
