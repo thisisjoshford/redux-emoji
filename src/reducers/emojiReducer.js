c11d59c01ffe53ba26c8516a269ea683a537b7d1
Josh Ford <thisisjoshford@gmail.com>
2020-05-12 16:20:13 -0700

    eats a snack test passing!

--- src/reducers/emojiReducer.js
+++ src/reducers/emojiReducer.js
@@ -1,9 +1,14 @@
-const initialState = { coffee: 0 };
+const initialState = { 
+  coffee: 0, 
+  snacks: 0
+};
 
 export default function emojiReducer(state = initialState, action) {
   switch(action.type) {
     case 'DRINK_COFFEE':
       return { coffee: state.coffee + 1 };
+    case 'EAT_SNACK':
+      return { snacks: state.snacks + 1 };
   }
 }
 
