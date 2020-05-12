ba3f898a420c3a9c9133533978120f01582b065a
Josh Ford <thisisjoshford@gmail.com>
2020-05-12 16:23:21 -0700

    takes a nap test passing!

--- src/reducers/emojiReducer.js
+++ src/reducers/emojiReducer.js
@@ -10,6 +10,8 @@ export default function emojiReducer(state = initialState, action) {
       return { coffee: state.coffee + 1 };
     case 'EAT_SNACK':
       return { snacks: state.snacks + 1 };
+    case 'TAKE_NAP':
+      return { naps: state.naps + 1 };
   }
 }
 
