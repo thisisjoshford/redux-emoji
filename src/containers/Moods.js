74cef0f257e13e1c237cf0b5e54c1a9450e37a2c
Josh Ford <thisisjoshford@gmail.com>
2020-05-12 23:00:11 -0700

    added dispatchers

--- src/containers/Moods.js
+++ src/containers/Moods.js
@@ -1,64 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Controls from '../components/controls/Controls';
 import Face from '../components/face/Face';
+import { drinkCoffee, eatSnack, takeNap, study } from '../actions/emojiActions';
 
-export const isTired = state => state.coffees < 1 && state.naps < 1;
-export const isHyper = state => state.coffees > 3;
-export const isEducated = state => state.studies > 2;
-export const isHungry = state => state.snacks < 1;
 
-export const getFace = state => {
-  if(isTired(state) && isHungry(state)) return '🤬';
-  if(isHyper(state) && isHungry(state)) return '🤮';
-  if(isTired(state)) return '😴';
-  if(isHyper(state)) return '🙀';
-  if(isEducated(state)) return '🤯';
-  if(isHungry(state)) return '😡';
+const Moods = () => {
+  const dispatch = useDispatch();
 
-  return '😀';
+  // const { coffees, snacks, naps, studies } = this.state;
+  const face = getFace(this.state);
+
+  return (
+    <>
+      <Controls>
+        <button 
+          onClick={() => dispatch(drinkCoffee())}
+        >coffee - {coffees}
+        </button>
+        <button 
+          onClick={() => dispatch(eatSnack())}
+        >snacks - {snacks}
+        </button>
+        <button 
+          onClick={() => dispatch(takeNap())}
+        >naps - {naps}
+        </button>
+        <button onClick={() => dispatch(study())}
+        >studies - {studies}
+        </button>
+      </Controls>
+      <Face emoji={face} />
+    </>
+  );
 };
-
-export default class Moods extends Component {
-  state = {
-    coffees: 0,
-    snacks: 0,
-    naps: 0,
-    studies: 0
-  }
-
-  handleSelection = action => {
-    switch(action.type) {
-      case 'DRINK_COFFEE':
-        this.setState(state => ({ coffees: state.coffees + 1 }));
-        break;
-      case 'EAT_SNACK':
-        this.setState(state => ({ snacks: state.snacks + 1 }));
-        break;
-      case 'TAKE_NAP':
-        this.setState(state => ({ naps: state.naps + 1 }));
-        break;
-      case 'STUDY':
-        this.setState(state => ({ studies: state.studies + 1 }));
-        break;
-      default:
-        console.log(`unhandled type: ${action.type}`);
-    }
-  }
-
-  render() {
-    const { coffees, snacks, naps, studies } = this.state;
-    const face = getFace(this.state);
-
-    return (
-      <>
-        <Controls>
-          <button onClick={() => this.handleSelection({ type: 'DRINK_COFFEE' })}>coffee - {coffees}</button>
-          <button onClick={() => this.handleSelection({ type: 'EAT_SNACK' })}>snacks - {snacks}</button>
-          <button onClick={() => this.handleSelection({ type: 'TAKE_NAP' })}>naps - {naps}</button>
-          <button onClick={() => this.handleSelection({ type: 'STUDY' })}>studies - {studies}</button>
-        </Controls>
-        <Face emoji={face} />
-      </>
-    );
-  }
-}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
+import emojis from './emojiReducer';
 
 export default combineReducers({
-
+  emojiState: emojis 
+  //timerState: timer
 });
